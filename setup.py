6723565cc92b9cf93185fd0c7d85c4782506e449
Fuzen <fuzen@protonmail.com>
2016-09-12 10:35:50 -0400

    Cleanup
    Cleaned up code some, added todo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Novelty',
-    version='1.0.2',
+    version='1.0.3',
     packages=['Novelty'],
     url='https://github.com/Fuzen-py/Novelty',
     license='MIT',
